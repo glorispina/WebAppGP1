ac520b6a5bcb9db978a4ff68cac63dc9c70467e4
glorispina <47580334+glorispina@users.noreply.github.com>
2019-06-10 08:42:17 -0400

    Add project files.

---
+++ WebAppGP1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppGP1.MvcApplication" Language="C#" %>
---
+++ WebAppGP1/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebAppGP1.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Finished Reading Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime FinishedReadingDate { get; set; }
+    }
+
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
